6e8bc8c60cd4b5abededf617a1340b3ff7e17f36
James Ballard <jimbonda@gmail.com>
2013-12-02 13:13:14 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-moodle-local_validatelang
+Moodle Language Validation
 =========================
 
-Prints all language strings from a given component on one screen to validate HTML and run extra checks for case (e.g. sentence case as used by most of Moodle).
+This is a developer tool that will prints all language strings from a given component on one screen to allow HTML validation and run extra checks for case (e.g. sentence case as used by most of Moodle).
+
+##Use
+
+1. Checkout the language strings via Site Administration -> Language -> Language Customisation
+2. Edit local/validatelang/index.php to use the desired file
+3. Go to http://your/moodle/local/validatelang/
+
+##Limitations/To-do
+
+1. Make component selectable
+2. Extend navigation to appear under language settings
+3. Provide options for different case/grammar analysis
+4. Provide auto W3C validation of strings
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once('../../config.php');
+global $DB, $OUTPUT, $PAGE;
+
+$PAGE->set_context(null);
+require_login();
+
+$PAGE->set_pagelayout('admin');
+$PAGE->set_url('/local/validatelang/index.php');
+$PAGE->set_title($SITE->shortname);
+
+$PAGE->navbar->add(get_string('admin);
+
+// This is hard-coded to a particular component for now - use DB to find it.
+$strings = $DB->get_records('tool_customlang', array('componentid' => 1));
+
+echo $OUTPUT->header();
+
+echo $OUTPUT->heading('strings');
+
+foreach ($strings as $string) {
+    echo $OUTPUT->box_start();
+    $langstring = get_string($string->stringid, 'enrol_avetmiss');
+    $casedstring = local_validatelang_sentence_case($langstring);
+    echo $langstring;
+    $casecompare = strcmp($casedstring, $langstring);
+    if (!empty($casecompare)) {
+        echo html_writer::tag('p', 'Wrong case', array('class' => 'alert alert-error'));
+    }
+    echo $OUTPUT->box_end();
+}
+
+echo $OUTPUT->footer();
+
+function local_validatelang_sentence_case($str) {
+    $cap = true;
+    $ret='';
+    $str = strtolower($str);
+    for ($x = 0; $x < strlen($str); $x++) {
+        $letter = substr($str, $x, 1);
+        if ($letter == "." || $letter == "!" || $letter == "?") {
+            $cap = true;
+        } else if ($letter != " " && $cap == true) {
+            $letter = strtoupper($letter);
+            $cap = false;
+        }
+        $ret .= $letter;
+    }
+    return $ret;
+}
+
+?>
